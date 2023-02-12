77e387f369be1b18202748f248a355560b046f49
tanikura <122531271+tanikura@users.noreply.github.com>
2023-02-12 10:20:05 +0000

    [Finish] 2-10

--- new.rb
+++ new.rb
@@ -1,16 +1,16 @@
 # インスタンスメソッド
 class Car
-  
+
   def move(direction, distance)
     self.turn(direction)
     self.run(distance)
   end
-  
-  
+
+
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
-  
+
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
